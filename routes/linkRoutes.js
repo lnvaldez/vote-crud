888d7ac76df9d1d0cc8f326aae2c355cb268197e
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 18:08:25 -0300

    Include put method for link voting function

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/", controller.addLink);
 router.get("/:theme_id", controller.getThemeLinks);
 router.put("/", controller.updateLink);
+router.put("/vote/:link_id", controller.voteForLink);
 router.delete("/:id", controller.deleteLink);
 
 module.exports = router;
