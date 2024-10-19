fe7f36402acd5aa6fef4b14dd735bc24922ceafb
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 14:56:41 -0300

    Execute query with passed arguments

--- controllers/themeController.js
+++ controllers/themeController.js
@@ -2,4 +2,8 @@ const Theme = require("../models/Theme");
 
 exports.createTheme = async (req, res) => {
   const { title, description, img_url, user_id } = req.body;
+
+  try {
+    await Theme.createTheme(title, description, img_url, user_id);
+  } catch (error) {}
 };
