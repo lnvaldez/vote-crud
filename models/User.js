2fb04b3441e0ca3e5e70bfd3bdbbe0ba83c95aa3
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 16:07:26 -0300

    Use bcrypt to hash a password

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const pool = require("../config/config");
+const bcrypt = require("bcrypt");
+
+exports.createUser = async (username, email, password) => {
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    const query = "INSERT INTO users (username, email, hash) VALUES (?, ?, ?)";
+
+    await pool.execute(query, [username, email, hash]);
+    console.log("User created");
+  } catch (error) {
+    console.error("Failed to create user");
+  }
+};
