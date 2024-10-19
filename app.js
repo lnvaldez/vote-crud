088a8b6653597563dab184b53266da9e8d4ab1a0
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 16:28:24 -0300

    Include user routes on app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const themeRoutes = require("./routes/themeRoutes");
 const linkRoutes = require("./routes/linkRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/themes", themeRoutes);
 app.use("/links", linkRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Running app on port ${PORT}`);
