a24e1443138b90162ceff53136aed4ad9227f19b
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 17:51:55 -0300

    Update path for vote route

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/", controller.createTheme);
 router.get("/", controller.getAllThemes);
 router.put("/", controller.updateTheme);
-router.put("/:theme_id", controller.voteForTheme);
+router.put("/vote/:theme_id", controller.voteForTheme);
 router.delete("/:id", controller.deleteTheme);
 
 module.exports = router;
