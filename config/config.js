067a7c22a276928ff0f93713d8983b3eef2769d9
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 14:45:13 -0300

    Create promise based mysql pool (connection)

--- config/config.js
+++ config/config.js
@@ -2,3 +2,10 @@ const dotenv = require("dotenv");
 const mysql = require("mysql2/promise");
 
 dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
