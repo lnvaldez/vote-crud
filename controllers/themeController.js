7fe82f253d1c5e03f1c0ea05f9b5832408c07250
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 14:57:36 -0300

    Response status 201 for succesful query execution

--- controllers/themeController.js
+++ controllers/themeController.js
@@ -5,5 +5,6 @@ exports.createTheme = async (req, res) => {
 
   try {
     await Theme.createTheme(title, description, img_url, user_id);
+    res.status(201).json({ message: "Theme created" });
   } catch (error) {}
 };
