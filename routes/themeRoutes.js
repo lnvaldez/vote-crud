05de8d937cbb74ab1a838de864a38010ba95bf8f
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 20:33:12 -0300

    Change theme voting method to post

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/", controller.createTheme);
 router.get("/", controller.getAllThemes);
 router.put("/:theme_id", controller.updateTheme);
-router.put("/vote/:theme_id", controller.voteForTheme);
+router.post("/:theme_id/vote", controller.voteForTheme);
 router.delete("/:id", controller.deleteTheme);
 
 module.exports = router;
