d414517d8ae3d793bb9c194d9df578b0e854550f
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 16:46:04 -0300

    Define router method as get for getAllThemes function

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/themeController");
 const router = express.Router();
 
 router.post("/", controller.createTheme);
+router.get("/", controller.getAllThemes);
 router.delete("/:id", controller.deleteTheme);
 
 module.exports = router;
