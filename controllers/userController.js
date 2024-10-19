d2b02295365e2c284df74c49ed1ef6f9dc63b34a
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 16:19:05 -0300

    'createUser' function creates a new user based on payload

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    await User.createUser(username, email, password);
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create user" });
+  }
+};
