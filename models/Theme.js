4072282b7ee1c2e1a1701169725ffc32e2c7e7d9
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 14:50:05 -0300

    'createTheme' function attempts to execute insert query for theme

--- models/Theme.js
+++ models/Theme.js
@@ -3,4 +3,11 @@ const pool = require("../config/config");
 exports.createTheme = async (title, description, img_url, user_id) => {
   const query =
     "INSERT INTO themes (title, description, img_url, user_id) VALUES (?, ?, ?, ?)";
+
+  try {
+    await pool.execute(query, [title, description, img_url, user_id]);
+    console.log("Theme created");
+  } catch (error) {
+    console.error("Failed to create theme");
+  }
 };
