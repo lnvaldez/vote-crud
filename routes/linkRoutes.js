37edde2bdb2b5f972d74416de21c21fea7c0e3ef
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 16:56:43 -0300

    Define dynamic parameter to get links associated to theme

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/linkController");
 const router = express.Router();
 
 router.post("/", controller.addLink);
+router.get("/:theme_id", controller.getThemeLinks);
 router.delete("/:id", controller.deleteLink);
 
 module.exports = router;
