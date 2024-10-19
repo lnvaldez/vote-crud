29e623e0df4d4f5e76c28f790a8b7fc79d1a4e23
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 15:51:00 -0300

    Define path for theme and link routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const themeRoutes = require("./routes/themeRoutes");
+const linkRoutes = require("./routes/linkRoutes");
 
 dotenv.config();
 
@@ -7,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
+
+app.use("/themes", themeRoutes);
+app.use("/links", linkRoutes);
 
 app.listen(PORT, () => {
   console.log(`Running app on port ${PORT}`);
