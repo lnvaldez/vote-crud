954318ea8650791645e402e70c204f4b52160292
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 14:49:01 -0300

    Define sql statement to insert theme

--- models/Theme.js
+++ models/Theme.js
@@ -1,1 +1,6 @@
 const pool = require("../config/config");
+
+exports.createTheme = async (title, description, img_url, user_id) => {
+  const query =
+    "INSERT INTO themes (title, description, img_url, user_id) VALUES (?, ?, ?, ?)";
+};
