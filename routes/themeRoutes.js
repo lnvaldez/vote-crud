20d1e9637bdb1f4b61e0d940836f7b61776a7531
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-19 15:46:32 -0300

    Define functions to be executed on route request

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -3,4 +3,7 @@ const controller = require("../controllers/themeController");
 
 const router = express.Router();
 
+router.post("/", controller.createTheme);
+router.delete("/:id", controller.deleteTheme);
+
 module.exports = router;
