66918c786c42822646b1bf7877be5f49d86ee6d3
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 16:22:49 -0300

    Change route path for theme creation post method

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -12,11 +12,11 @@ router.get("/", controller.getAllThemes);
 router.get("/create", requireAuth, controller.renderCreateTheme);
 router.post("/create", requireAuth, controller.createTheme);
 
-router.get("/my-themes", requireAuth, controller.getMyThemes);
-router.get("/edit/:id", requireAuth, controller.renderEditPage);
-router.post("/edit/:id", requireAuth, controller.updateTheme);
-router.post("/delete/:id", requireAuth, controller.deleteTheme);
+// router.get("/my-themes", requireAuth, controller.getMyThemes);
+// router.get("/edit/:id", requireAuth, controller.renderEditPage);
+// router.post("/edit/:id", requireAuth, controller.updateTheme);
+// router.post("/delete/:id", requireAuth, controller.deleteTheme);
 
-router.post("/:theme_id/vote", controller.voteForTheme);
+// router.post("/:theme_id/vote", controller.voteForTheme);
 
 module.exports = router;
