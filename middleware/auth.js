0660eea851800745396284eb8ff018830e15acbc
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 11:33:30 -0300

    Verify if token is valid

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,4 +6,8 @@ exports.auth = (req, res, next) => {
   if (!token) {
     return res.status(401).json({ message: "Not authorized" });
   }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {}
 };
