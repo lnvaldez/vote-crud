75f8149a44a272cded1f169c41140e98f1f1ce6c
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 15:58:28 -0300

    Theme routes require user from locals provided by auth

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const controller = require("../controllers/themeController");
-const { auth } = require("../middleware/auth");
+const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/create", auth, controller.renderCreateTheme);
-router.post("/", auth, controller.createTheme);
+router.get("/", requireAuth, controller.getAllThemes);
 
-router.get("/", controller.getAllThemes);
+router.get("/create", requireAuth, controller.renderCreateTheme);
+router.post("/", requireAuth, controller.createTheme);
+
 router.put("/:theme_id", controller.updateTheme);
 router.post("/:theme_id/vote", controller.voteForTheme);
 router.delete("/:id", controller.deleteTheme);
