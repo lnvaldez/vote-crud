a9338abed218955c71616b89852ceda07ed98d9e
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 14:42:21 -0300

    Add /create path for theme creation page rendering

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -4,7 +4,9 @@ const { auth } = require("../middleware/auth");
 
 const router = express.Router();
 
+router.get("/create", auth, controller.renderCreateTheme);
 router.post("/", auth, controller.createTheme);
+
 router.get("/", controller.getAllThemes);
 router.put("/:theme_id", controller.updateTheme);
 router.post("/:theme_id/vote", controller.voteForTheme);
