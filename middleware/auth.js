53b3dd9d6a7f9b656770914a52d5eef8e2ff2826
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 11:30:24 -0300

    Extract token from request cookies

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-exports.auth = (req, res, next) => {};
+exports.auth = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+};
