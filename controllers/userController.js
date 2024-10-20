8d2934a95b1d76203214a95fc167600742956428
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 11:23:47 -0300

    Include dotenv to load jwt secret from .env file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
