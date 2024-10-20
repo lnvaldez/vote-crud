791e573e1a4bcd6e9b17e78077a3663866a9e993
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 12:32:04 -0300

    Redirect to login page after registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,8 @@ exports.register = async (req, res) => {
 
   try {
     await User.createUser(username, email, password);
-    res.status(201).json({ message: "User created" });
+    res.redirect("login");
+    // res.status(201).json({ message: "User created" });
   } catch (error) {
     res.status(500).json({ error: "Failed to create user" });
   }
