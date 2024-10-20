24e02a1a645e82f666909f4c111def46c86e4b16
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 16:05:57 -0300

    Declare public/protected routes and define future routes

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -4,13 +4,19 @@ const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
+// Public routes
 router.get("/", controller.getAllThemes);
+// router.get("/view/:id", controller.getTheme);
 
+// Protected routes
 router.get("/create", requireAuth, controller.renderCreateTheme);
-router.post("/", requireAuth, controller.createTheme);
+router.post("/create", requireAuth, controller.createTheme);
 
-router.put("/:theme_id", controller.updateTheme);
+router.get("/my-themes", requireAuth, controller.getMyThemes);
+router.get("/edit/:id", requireAuth, controller.renderEditPage);
+router.post("/edit/:id", requireAuth, controller.updateTheme);
+router.post("/delete/:id", requireAuth, controller.deleteTheme);
+
 router.post("/:theme_id/vote", controller.voteForTheme);
-router.delete("/:id", controller.deleteTheme);
 
 module.exports = router;
