cc044e37479d2fdd4187e7f856a710529fd5025c
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-20 12:11:30 -0300

    Add get method for register rendering route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,9 @@ const controller = require("../controllers/userController");
 
 const router = express.Router();
 
+router.get("/register", controller.renderRegisterPage);
 router.post("/register", controller.register);
+
 router.post("/login", controller.login);
 router.post("/logout", controller.logout);
 
