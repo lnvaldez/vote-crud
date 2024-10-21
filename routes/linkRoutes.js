5acb23c54342d7df89cb4b7eab81b616aef57c03
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-21 13:52:23 -0300

    Change route path name and method

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/linkController");
 const router = express.Router();
 
 router.post("/add", controller.addLink);
-router.put("/:link_id", controller.updateLink);
+router.post("/edit/:link_id", controller.updateLink);
 router.post("/:link_id/vote", controller.voteForLink);
 router.post("/delete/:id", controller.deleteLink);
 
