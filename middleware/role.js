9b15ad9968bcf1bbd49a5561d615625f8af373c5
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-26 15:24:28 -0300

    Function to check and authorize based on role

--- middleware/role.js
+++ middleware/role.js
@@ -1,8 +1,16 @@
-exports.authorizeRole = (role) => {
-  return (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).send("Access denied");
-    }
-    next();
-  };
+const User = require("../models/User");
+
+exports.authorizeRole = (role) => (req, res, next) => {
+  if (!req.user || req.user.role !== role) {
+    return res.status(403).send("Access denied");
+  }
+  next();
+};
+
+exports.checkRole = async (req, res, next) => {
+  if (req.user && req.user.id) {
+    const result = await User.getRole(req.user.id);
+    req.user.role = result.role;
+  }
+  next();
 };
