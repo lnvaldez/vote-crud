081fb62e54a5332dcf6c247a2fa08b49ae63c676
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-26 14:36:10 -0300

    Logic to render admin page with pending themes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,10 @@
 const Theme = require("../models/Theme");
 
 exports.renderAdminPage = async (req, res) => {
-  const themes = res.render("/pages/admin-panel");
+  try {
+    const themes = await Theme.getPendingThemes();
+    res.render("pages/admin-panel", { themes });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to render admin panel" });
+  }
 };
