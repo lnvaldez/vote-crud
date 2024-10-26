491d1e1e2fe71a82d6ec1c7c984ec5e6f384a3d4
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-26 15:25:56 -0300

    Render admin user page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,15 @@
 const Theme = require("../models/Theme");
 
-exports.renderAdminPage = async (req, res) => {
+exports.renderAdminThemePage = async (req, res) => {
   try {
     const themes = await Theme.getPendingThemes();
     res.render("pages/admin-panel", { themes });
   } catch (error) {
     res.status(500).json({ error: "Failed to render admin panel" });
   }
+};
+
+exports.renderAdminUserPage = async (req, res) => {
+  try {
+  } catch (error) {}
 };
