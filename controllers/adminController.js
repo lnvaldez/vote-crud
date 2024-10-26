e1e435b7afd1fa611ed558788258d4e7771da171
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-26 15:46:30 -0300

    Rename rendered pages and write admin user page logic

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,20 @@
 const Theme = require("../models/Theme");
+const User = require("../models/User");
 
 exports.renderAdminThemePage = async (req, res) => {
   try {
     const themes = await Theme.getPendingThemes();
-    res.render("pages/admin-panel", { themes });
+    res.render("pages/admin-themes", { themes });
   } catch (error) {
-    res.status(500).json({ error: "Failed to render admin panel" });
+    res.status(500).json({ error: "Failed to render admin theme page" });
   }
 };
 
 exports.renderAdminUserPage = async (req, res) => {
   try {
-  } catch (error) {}
+    const users = await User.getAllUsers();
+    res.render("pages/admin-users", { users });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to render admin user page" });
+  }
 };
