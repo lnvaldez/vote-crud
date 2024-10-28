1bbee6e50bf8afe16a4a8a587360d22a719a0963
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-28 10:46:56 -0300

    Logic for session model

---
+++ models/Session.js
@@ -0,0 +1,36 @@
+const session = require("express-session");
+const pool = require("../config/config");
+
+exports.storeSession = async (sessionId, userId, expiresAt) => {
+  const query =
+    "INSERT INTO sessions (session_id, user_id, expires_at) VALUES (?, ?, ?)";
+
+  try {
+    await pool.query(query, [sessionId, userId, expiresAt]);
+    console.log("Session stored");
+  } catch (error) {
+    console.error("Failed to store session");
+  }
+};
+
+exports.expireSession = async (sessionId) => {
+  const query = "UPDATE sessions SET is_valid = false WHERE session_id = ?";
+
+  try {
+    await pool.query(query, [sessionId]);
+    console.log("Session marked as invalid");
+  } catch (error) {
+    console.error("Failed to mark session as invalid");
+  }
+};
+
+exports.deleteSession = async (sessionId) => {
+  const query = "DELETE FROM sessions WHERE session_id = ?";
+
+  try {
+    await pool.query(query, [sessionId]);
+    console.log("Session deleted");
+  } catch (error) {
+    console.error("Failed to delete session");
+  }
+};
