be4beb4dddabb38505d4d9827ec8311af2205852
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-29 14:37:56 -0300

    Setup transporter configuration for nodemailer

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,15 @@
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
-const sendEmail = (option) => {};
+const sendEmail = (option) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  });
+};
 
 module.exports = sendEmail;
