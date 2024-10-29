527bd1a1ce69ceb2cf23f1e1bf3ad7774931ec7b
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-29 11:39:27 -0300

    Move reset token generation to utils folder

---
+++ utils/resetToken.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+const crypto = require("crypto");
+
+exports.createResetPasswordToken = async () => {
+  const resetToken = crypto.randomBytes(32, toString("hex"));
+
+  const hashedResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const expireDate = Date.now() + 10 * 60 * 1000;
+
+  console.log(hashedResetToken, expireDate);
+
+  try {
+    await User.storeResetData(hashedResetToken, expireDate);
+    return resetToken;
+  } catch (error) {
+    console.error("Failed to store password reset data");
+  }
+};
